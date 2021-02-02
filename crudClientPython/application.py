0ac14a983d9b9cc862a07feb5965695301d860d3
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-02 10:10:07 -0300

    alteração na classe application.py modificação no metodo inserir

--- crudClientPython/application.py
+++ crudClientPython/application.py
@@ -4,23 +4,23 @@ import Backend as core
 
 app = None
 
-def view_command()
+def view_command():
     rows =  core.view()
     app.listClientes.delete(0, END)
     for r in rows:
         app.listClientes.insert(END, r)
 
-def search_command()
+def search_command():
     app.listClientes.delete(0, END)        
     rows = core.search(app.txtNome.get(), app.txtSobrenome.get(), app.txtEmail.get(), app.txtCPF.get())
     for r in rows:
         app.listClientes.insert(END, r)
 
-def insert_command()
-    core.insert(app.txtNome.get(), app.txtSobrenome.get(), app.txtEmail.get(), app.txtEmail.get(), app.txtCPF.get())     
+def insert_command():
+    core.insert(app.txtNome.get(), app.txtSobrenome.get(), app.txtEmail.get(), app.txtCPF.get())     
     view_command()
 
-def getSelectedRow(event)        
+def getSelectedRow(event):     
     global selected 
     index = app.listClientes.curselection()[0]
     selected = app.listClientes.get(index)
@@ -32,16 +32,18 @@ def getSelectedRow(event)
     app.entEmail.insert(END,selected[3])
     app.entCPF.delete(0, END)
     app.entCPF.insert(END, selected[4])
+    return selected
 
-def update_command();
+def update_command():
     core.update(selected[0], app.txtNome.get(), app.txtSobrenome.get(), app.txtEmail.get(), app.txtCPF.get())
     view_command()
 
-def del_command()
+def del_command():
     id = selected[0]    
     core.delete(id)
     view_command()
 
+if __name__ == "__main__":
     app = Gui()
     app.listClientes.bind('<<ListboxSelect>>', getSelectedRow)
     app.btnViewAll.configure(command=view_command)
